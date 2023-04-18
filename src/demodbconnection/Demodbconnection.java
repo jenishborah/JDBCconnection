f9b39703ab09cf330857d8641092df6a20cdae7e
Jenish <63183649+jenishborah@users.noreply.github.com>
2023-04-18 13:12:07 +0530

    Add files via upload

---
+++ src/demodbconnection/Demodbconnection.java
@@ -0,0 +1,27 @@
+
+
+import java.sql.*;
+public class Demodbconnection {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try
+        {
+        Class.forName("com.mysql.jdbc.Driver");
+        Connection con=DriverManager.getConnection(  
+        "jdbc:mysql://localhost:3306/dept","Jenish","Jenish@3116");  
+ 
+        Statement stmt=con.createStatement();  
+        ResultSet rs=stmt.executeQuery("select * from student");  
+        while(rs.next())  
+        System.out.println(rs.getInt(1)+"  "+rs.getString(2)+"  "+rs.getString(3));  
+        con.close();  
+}
+        catch(Exception e)
+        { 
+            System.out.println(e);
+        }  
+}  
+}  
